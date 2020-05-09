1876a8596e577dafa298cfa41f4861728da238ed
fengxing <f296016140@gmail.com>
2020-05-09 23:39:06 +0800

    ss

--- app/src/main/java/com/zhyen/android/test/test_interview/ArrayTreeNode.java
+++ app/src/main/java/com/zhyen/android/test/test_interview/ArrayTreeNode.java
@@ -1,4 +1,32 @@
 package com.zhyen.android.test.test_interview;
 
-public class ArrayTreeNode {
+public class ArrayTreeNode<T> {
+
+    private T mData;
+    private int mParentIndex;
+
+    public ArrayTreeNode() {
+
+    }
+
+    public ArrayTreeNode(T data, int index) {
+        this.mData = data;
+        this.mParentIndex = index;
+    }
+
+    public T getData() {
+        return mData;
+    }
+
+    public void setData(T mData) {
+        this.mData = mData;
+    }
+
+    public int getParentIndex() {
+        return mParentIndex;
+    }
+
+    public void setParentIndex(int mParentIndex) {
+        this.mParentIndex = mParentIndex;
+    }
 }
