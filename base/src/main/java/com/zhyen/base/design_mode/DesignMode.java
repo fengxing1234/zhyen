04db6388489d43cc58c364e1f22fb5847e62f34d
fengxing <f296016140@gmail.com>
2020-05-15 13:27:58 +0800

    test(base): 组合模式

--- base/src/main/java/com/zhyen/base/design_mode/DesignMode.java
+++ base/src/main/java/com/zhyen/base/design_mode/DesignMode.java
@@ -39,6 +39,8 @@ import com.zhyen.base.design_mode.command_mode.UML.HeFenCommand;
 import com.zhyen.base.design_mode.command_mode.UML.HunTunCommand;
 import com.zhyen.base.design_mode.command_mode.UML.Invoke;
 import com.zhyen.base.design_mode.command_mode.UML.Waiter;
+import com.zhyen.base.design_mode.composite_mode.Bags;
+import com.zhyen.base.design_mode.composite_mode.Goods;
 import com.zhyen.base.design_mode.decorator_mode.ConcreteComponent;
 import com.zhyen.base.design_mode.decorator_mode.ConcreteDecorator;
 import com.zhyen.base.design_mode.decorator_mode.IComponent;
@@ -145,7 +147,38 @@ public class DesignMode {
         //外观模式
         //facadeMode();
         //响元模式
-        flyweightMode();
+        //flyweightMode();
+        //组合模式
+        compositeMode();
+    }
+
+    private static void compositeMode() {
+        float s = 0;
+        Bags BigBag, mediumBag, smallRedBag, smallWhiteBag;
+        Goods sp;
+        BigBag = new Bags("大袋子");
+        mediumBag = new Bags("中袋子");
+        smallRedBag = new Bags("红色小袋子");
+        smallWhiteBag = new Bags("白色小袋子");
+        sp = new Goods("婺源特产", 2, 7.9f);
+        smallRedBag.add(sp);
+        sp = new Goods("婺源地图", 1, 9.9f);
+        smallRedBag.add(sp);
+        sp = new Goods("韶关香菇", 2, 68);
+        smallWhiteBag.add(sp);
+        sp = new Goods("韶关红茶", 3, 180);
+        smallWhiteBag.add(sp);
+        sp = new Goods("景德镇瓷器", 1, 380);
+        mediumBag.add(sp);
+        mediumBag.add(smallRedBag);
+        sp = new Goods("李宁牌运动鞋", 1, 198);
+        BigBag.add(sp);
+        BigBag.add(smallWhiteBag);
+        BigBag.add(mediumBag);
+        System.out.println("您选购的商品有：");
+        BigBag.show();
+        s = BigBag.calculation();
+        System.out.println("要支付的总价是：" + s + "元");
     }
 
     private static void flyweightMode() {
---
+++ base/src/main/java/com/zhyen/base/design_mode/composite_mode/Articles.java
@@ -0,0 +1,9 @@
+package com.zhyen.base.design_mode.composite_mode;
+
+//抽象构件：物品
+public interface Articles {
+
+    public float calculation(); //计算
+
+    public void show();
+}
